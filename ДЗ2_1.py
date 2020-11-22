5631e6016f25e3eadeba17fd7204c61a4ba22e69
irabadzhy <74873722+irabadzhy@users.noreply.github.com>
2020-11-22 20:21:03 +0200

    Add files via upload

---
+++ ДЗ2_1.py
@@ -0,0 +1,31 @@
+# value = input("введи число:")
+# value = int(value)
+#
+# if value % 5 == 0:
+#     print (value * 2)
+# else:
+#     print (0)
+# ###################################################
+# month = input('введите номер месяца 1-12:')
+# month = int(month)
+# if month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12 or month == 1:
+#     print ('в этом месяце 31 день')
+# elif month == 4 or month == 6 or month == 9 or month == 11:
+#     print ('в этом месяце 30 дней')
+# elif month == 2:
+#     print ('в этом месяце 28 дней')
+# else:
+#     print ("не верный номер месяца")
+# ###################################################
+#
+# month = input('введите название месяца что бы узнать сколько в нем дней:')
+# month = str(month)
+# if month == 'январь' or month == 'март' or month == 'май' or month == 'июль' or month == 'август' or month == 'октябрь' or month == 'декабрь':
+#     print ('в этом месяце 31 день')
+# elif month == 'апрель' or month == 'июнь' or month == 'сентябрь' or month == 'ноябрь':
+#     print ('в этом месяце 30 дней')
+# elif month == 'февраль':
+#     print('в этом месяце 28 дней')
+# else:
+#     print ('неверное название месяца')
+#
